de8feb0295ca04c3f5625385e59f15621e7c6d99
Seunghwan Lee <seunghwan369@naver.com>
2022-04-12 18:54:28 +0900

    first commit

--- 13. reservationPage/reservation.js
+++ 13. reservationPage/reservation.js
@@ -1,117 +1,130 @@
 //---------- 한글만 입력 -----------
-$("#text__ko1").keyup(function(event) {
+$("#text__ko1").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(
+      inputVal.replace(/[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/gi, "")
+    );
   }
 });
 
-$("#text__ko2").keyup(function(event) {
+$("#text__ko2").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(
+      inputVal.replace(/[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/gi, "")
+    );
   }
 });
 
 //---------- 영문만 입력 -----------
-$("#text__eng1").keyup(function(event) {
+$("#text__eng1").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^[a-zA-Z]*$/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^[a-zA-Z]*$/gi, ""));
   }
 });
 
-$("#text__eng2").keyup(function(event) {
+$("#text__eng2").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^[a-zA-Z]*$/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^[a-zA-Z]*$/gi, ""));
   }
 });
-
 
 //---------- 숫자만 입력 -----------
-$("#text__num1").keyup(function(event) {
+$("#text__num1").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
 
-$("#text__num2").keyup(function(event) {
+$("#text__num2").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
 
-$("#text__num3").keyup(function(event) {
+$("#text__num3").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
 
-$("#text__num4").keyup(function(event) {
+$("#text__num4").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
 
 //---------- 이메일 입력 -----------
-$("#text__email1").keyup(function(event) {
+$("#text__email1").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(
+      inputVal.replace(
+        /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/gi,
+        ""
+      )
+    );
   }
 });
 
-$("#text__email2").keyup(function(event) {
+$("#text__email2").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(
+      inputVal.replace(
+        /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/gi,
+        ""
+      )
+    );
   }
 });
 
 //********** 미체크 시 알림창 ***********
 function Checkform() {
-  if (frm.confirm.checked !=true){
-      alert("예약내역 확인 후 체크해주세요.");
-      frm.confirm.focus();
-      return false;
-    }
+  if (frm.confirm.checked != true) {
+    alert("예약내역 확인 후 체크해주세요.");
+    frm.confirm.focus();
+    return false;
+  }
 }
 
 // ********** 운임 정보 ***********
 function onpenCloseTog() {
-  if(document.getElementById('toggle__content').style.display === 'block') {
-    document.getElementById('toggle__content').style.display = 'none';
-    document.getElementById('toggle').textContent = '';
+  if (document.getElementById("toggle__content").style.display === "block") {
+    document.getElementById("toggle__content").style.display = "none";
+    document.getElementById("toggle").textContent = "";
   } else {
-    document.getElementById('toggle__content').style.display = 'block';
-    document.getElementById('toggle').textContent = '';
+    document.getElementById("toggle__content").style.display = "block";
+    document.getElementById("toggle").textContent = "";
   }
 }
 
 // ********** 자동 입력 ***********
-function check1(f){
+function check1(f) {
   if (f.checked) {
-    $('#text__ko2').val($('#text__ko1').val());
+    $("#text__ko2").val($("#text__ko1").val());
 
-    $('#text__num3').val($('#text__num1').val());
+    $("#text__num3").val($("#text__num1").val());
 
-    $('#text__email2').val($('#text__email1').val());
+    $("#text__email2").val($("#text__email1").val());
 
-    $('#text__num4').val($('#text__num2').val());
-  } else{
-      $('#text__ko2').val('');
+    $("#text__num4").val($("#text__num2").val());
+  } else {
+    $("#text__ko2").val("");
 
-      $('#text__num3').val('');
+    $("#text__num3").val("");
 
-      $('#text__email2').val('');
+    $("#text__email2").val("");
 
-      $('#text__num4').val('');
-    }
+    $("#text__num4").val("");
+  }
 }
 
 // function agreeCheck(frm)
--- 2. idPage/id.js
+++ 2. idPage/id.js
@@ -3,69 +3,69 @@ var iptPhoneA = $("#cust__phone__a");
 var iptPhoneB = $("#cust__phone__b");
 var iptPhoneC = $("#cust__phone__c");
 
-iptPhoneA.on("keyup", function(){
-	if ($(this).val().length == 3) {
-		iptPhoneB.focus();
-	}
+iptPhoneA.on("keyup", function () {
+  if ($(this).val().length == 3) {
+    iptPhoneB.focus();
+  }
 });
-iptPhoneB.on("keyup", function(){
-	if ($(this).val().length == 4) {
-		iptPhoneC.focus();
-		iptPhoneC.attr("maxlength","5");
-	}
+iptPhoneB.on("keyup", function () {
+  if ($(this).val().length == 4) {
+    iptPhoneC.focus();
+    iptPhoneC.attr("maxlength", "5");
+  }
 });
-iptPhoneC.on("keyup", function(){
-	if ($(this).val().length == 5) {
-		if (iptPhoneB.val().length == 3) {
-			iptPhoneB.val(iptPhoneB.val() + $(this).val().substr(0,1));
-			iptPhoneC.val($(this).val().substr(1,4));
-			iptPhoneC.attr("maxlength","4");
-		} else if (iptPhoneC.val().length == 5) {
-			iptPhoneC.val($(this).val().substr(0,4));
-		}
-	}
-});
-
-//---------- 한/영만 입력 -----------
-$("#cust__name").keyup(function(event) {
-  if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^(ㄱ-힣a-zA-Z)]/gi, ''));
+iptPhoneC.on("keyup", function () {
+  if ($(this).val().length == 5) {
+    if (iptPhoneB.val().length == 3) {
+      iptPhoneB.val(iptPhoneB.val() + $(this).val().substr(0, 1));
+      iptPhoneC.val($(this).val().substr(1, 4));
+      iptPhoneC.attr("maxlength", "4");
+    } else if (iptPhoneC.val().length == 5) {
+      iptPhoneC.val($(this).val().substr(0, 4));
+    }
   }
 });
 
-$("#cust__email__b").keyup(function(event) {
+//---------- 한/영만 입력 -----------
+$("#cust__name").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^A-Za-z]/ig, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^(ㄱ-힣a-zA-Z)]/gi, ""));
+  }
+});
+
+$("#cust__email__b").keyup(function (event) {
+  if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^A-Za-z]/gi, ""));
   }
 });
 
 //---------- 숫자만 입력 -----------
-$("#cust__phone__a").keyup(function(event) {
+$("#cust__phone__a").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
-$("#cust__phone__b").keyup(function(event) {
+$("#cust__phone__b").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
-$("#cust__phone__c").keyup(function(event) {
+$("#cust__phone__c").keyup(function (event) {
   if (!(event.keyCode >= 37 && event.keyCode <= 40)) {
-     var inputVal = $(this).val();
-     $(this).val(inputVal.replace(/[^0-9]/gi, ''));
+    var inputVal = $(this).val();
+    $(this).val(inputVal.replace(/[^0-9]/gi, ""));
   }
 });
 
 //---------- radio event -----------
-function div__OnOff(v, id){
-  if (v == "email"){
-    document.getElementById(id).style.display=""; //보여줌
-  }else{
-    document.getElementById(id).style.display="none"; //숨김
+function div__OnOff(v, id) {
+  if (v == "email") {
+    document.getElementById(id).style.display = ""; //보여줌
+  } else {
+    document.getElementById(id).style.display = "none"; //숨김
   }
 }
